0219effa730b002b03c33de8018fe335da361195
James Bay <jeb54321@users.noreply.github.com>
2017-10-03 13:18:11 -0400

    Tweaked for Python 3 and latest urllib

--- Tarentella.py
+++ Tarentella.py
@@ -8,10 +8,12 @@ James Bay, 11/23/2015 - Requires Python 3.4.3
 
 import os
 import logging
+import urllib.request
 
 # Constant definitions
 LOGGING_LEVEL = logging.INFO
 FORMAT = "%(asctime)s %(levelname)-3.3s %(name)-20.20s %(message)s"
+PASSWORD = ""
 
 # If the environment variable exists, use it to set logging level.
 if os.environ.get("LOGGING_LEVEL", "INFO") == "DEBUG":
@@ -37,8 +39,15 @@ def main():
         print("Try running SET_PASSWORDS.BAT")
         return
 
-    # This is the whole shebang.
-    # TBD
+    # Read URL
+    
+    url = "https://www.google.com/search?q=test"
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    req = urllib.request.Request(url, headers = headers)
+    response = urllib.request.urlopen(req)
+    the_page = response.read()
+    print( the_page )
 
     LOGGER.info("Tarentella.py exiting.")
     return
